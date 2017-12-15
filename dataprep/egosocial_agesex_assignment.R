8b4bf8e5a8ea418c58ca3db448e70667d81389c6
williamcioffi <william.cioffi@gmail.com>
2017-12-14 19:14:24 -0500

    merged in CK comments, edits to egosocial_agesex_assignment.R

--- dataprep/egosocial_agesex_assignment.R
+++ dataprep/egosocial_agesex_assignment.R
@@ -1,13 +1,55 @@
-###
-# data prep for right whale social
-#
-# ~wrc 20171201
+#######################################
+##                                   ##
+##   Right Whale Social Analysis     ##
+##                                   ##
+##          -data prep-              ##
+##                                   ##
+##   Prep the data recieved from     ## 
+##   North Atlantic Right Whale      ##
+##   Consortium and get it ready     ##
+##   for analysis by tidying it up   ##
+##   and assigning agesex classes    ##
+##                                   ##
+##  Will Cioffi + Christin Khan      ##
+##                                   ##
+##        December 14, 2017          ##
+#######################################
 
 
-### load files
+### prep Excel file received from Phil Hamilton
+# open data and Save As in Excel file with a new name
+# delete 'Calving Data' worksheet
+# delete 'Code Explanation' worksheet
+# delete top row header that says 'All sightings of identified whales from 1980 through 2016'
+# highlight all and remove shading and borders   
+# highlight top row and replace all spaces with nothing
+# highlight all and replace all spaces with a period
+# highlight all and replace all commas with a period
+# highlight all and replace all double periods with a single period
+# highlight all and replace all blanks with 'NA' values
+# delete column "Association Id"
+# delete column "Association Type"
+# delete column "Singleton"
+# delete column "Observer"
+# delete column "Area Code"
+# delete column "Letter"
+# delete any whales (EGNo) of unknown gender 'X'
+# moved "Latitude" and "Longitude" columns over to the left, and formatted to 5 decimal places
+# insert a RID column for row names
+# save
+# save as a .txt file
 
-dat  <- read.table("../data/4-Khan-data-calf-errors-fixed-nodups.csv"       , header = TRUE, sep = ',')
+# later we deleted some rows of bad CALF codes when it wasn't a calf + duplicate rows of data
+
+
+### load files wrc
+dat  <- read.table("../data/5-Khan-data-cleaned-up.csv", header = TRUE, sep = ',')
 cdat <- read.table("../data/2017-11-06-Khan-data-request-calving.csv", header = TRUE, sep = ',')
+
+### load files from Christin's computer
+# setwd("C:/Users/christin.khan/Documents/Projects/Social-Behavior/2-Data")
+# dat  <- read.table("5-Khan-data-cleaned-up.csv", header = TRUE, sep = ',')
+# cdat <- read.table("2017-11-06-Khan-data-request-calving.csv", header = TRUE, sep = ',')
 
 
 ### make dates
@@ -49,7 +91,7 @@ date_posix <- date_posix[oo]
 # calf until December 1st of birth year and after juv
 # considered adults if:
 # 	1. nine years or more have ellapsed since known birth year
-#	2. eight years or more have ellapsed from date of frist sighting (when birth year is unknown)
+#	2. eight years or more have ellapsed from date of first sighting (when birth year is unknown)
 #	3. Jan 1st of the year prior to giving birth to a calf
 # whales without a known birth year seen for less than eight years were excluded (unknown age class)
 # lactating:
@@ -76,7 +118,7 @@ ageclass_tmp[which(years_since_1calf >= -1)] <- "A"
 calfbehs <- c("CALFW/MOM", "CALFOFUNPHMOM", "CALF", "CALFW/OTHERS(S)", "CALFW/UNPH")
 mombehs  <- c("W/CALF", "W/CALFUNPH")
 
-# set up provisional mom and calf catagories based on the behavior column
+# set up provisional mom and calf categories based on the behavior column
 # these will be adjusted and filled in later
 allbehs  			<- strsplit(as.character(dat$Behaviors), "\\.")
 calfs_bybehavior    <- sapply(allbehs, function(l) any(l %in% calfbehs))
@@ -131,3 +173,66 @@ for(m in 1:nrow(cdat)) {
 
 dat[, 'lactating'] <- lactating_tmp
 dat[, 'ageclass'] <- ageclass_tmp
+
+
+### create new columns to help double check agesex classes
+dat[, 'yearminusbirthyear']  	<- dat$Year - dat$BirthYear
+dat[, 'yearminusfirstsight'] 	<- dat$Year - dat$FirstYearSighted 
+dat[, 'yearminusfirstcalving'] 	<- dat$Year - dat$FirstCalvingYear
+
+
+### make id based on agesex
+
+agesexid_tmp <- paste0(ageclass_tmp, dat$Gender)
+agesexid_tmp[agesexid_tmp == "AF" & lactating_tmp == "L"] <- "LF"
+agesexid_tmp[agesexid_tmp == "AF"] <- "NF"
+
+
+dat[, 'agesexid'] <- paste0(dat$EGNo, agesexid_tmp)
+
+
+### make a table of birthdays, firstsightings, and deaths
+# check to make sure that there are no typos in BithYear or FirstYearSighted
+# there should be only one unique value for each animal for both of values
+# watch the multinegative logic...
+dat_list <- split(dat, dat$EGNo)
+
+stopifnot(!any(sapply(dat_list, function(l) length(unique(l$BirthYear))) != 1))
+stopifnot(!any(sapply(dat_list, function(l) length(unique(l$FirstYearSighted))) != 1))
+
+# make births and first
+uids <- sort(unique(dat$EGNo))
+nids <- length(uids)
+
+births <- sapply(dat_list, function(l) unique(l$BirthYear))
+firsts <- sapply(dat_list, function(l) unique(l$FirstYearSighted))
+
+# look for deaths
+deadcodes <- grep("DEAD", allbehs)
+dat[, 'date'] <- date
+dat_deads <- dat[deadcodes, ]
+dat_deads_list <- split(dat_deads, dat_deads$EGNo)
+deaths_tmp <- do.call('c', lapply(dat_deads_list, function(l) min(l$date)))
+
+deaths <- vector(mode = "character", length = nids)
+deaths[1:length(deaths)] <- NA
+deaths[match(names(deaths_tmp), uids)] <- as.character(deaths_tmp)
+
+# calculate deaths using the 6 year rule
+lasts <- sapply(dat_list, function(l) max(l$Year, na.rm = TRUE))
+lastsdeath <- lasts + 6
+lastsdeath[!is.na(lastsdeath)] <- paste0(lastsdeath[!is.na(lastsdeath)], "-01-01")
+
+# kill if we know a real death date
+lastsdeath[!is.na(deaths)] <- NA
+
+# comebine lastsdeath and deaths
+deathdates <- lastsdeath
+deathdates[is.na(lastsdeath)] <- deaths[is.na(lastsdeath)]
+
+birthdeath <- data.frame(EGNo = uids, birthyear = births, firstyearsighted = firsts, lastyearsighted = lasts, lastsighteddeath = lastsdeath, knowndeathdate = deaths, deathdates = deathdates, stringsAsFactors = FALSE)
+write.table(birthdeath, "../data/birthdata.csv", row.names = FALSE, sep = ',')
+
+
+### remove calfs
+dat <- dat[dat$ageclass != "calf", ]
