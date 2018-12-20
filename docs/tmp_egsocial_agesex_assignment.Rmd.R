0fa23b231275732a75f96181cebaf4e88d87f9b5
williamcioffi <william.cioffi@gmail.com>
2018-12-20 12:54:38 -0500

    switching computers ive started a tmporary markdown file for marking up (down?) the data prep steps for later reintegrations into readme.Rmd

---
+++ docs/tmp_egsocial_agesex_assignment.Rmd.R
@@ -0,0 +1,9 @@
+---
+output: 
+  html_document:
+    highlight: pygments
+    theme: spacelab
+---
+
+this is a temporary document for marking up (down?) some of the data prep scripts for eg social. to be recombined at a later date with the master document: readme.Rmd.
+
